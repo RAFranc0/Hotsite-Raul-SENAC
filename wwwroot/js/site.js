3962e72c1f781b2bed5529a2121deda67ed4d42b
Raul <raul.franco266@gmail.com>
2023-11-23 13:38:15 -0300

    Primeiro envio

---
+++ wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(
+    function () {
+        $("#formCadastro").submit(function (e) {
+                e.preventDefault();
+                Cadastrar();
+             }
+        );
+    }
+);
+
+function Cadastrar() {
+    var parametros =
+    {
+        Nome: $("#idNome").val(),
+        Email: $("#idEmail").val(),
+        Mensagem: $("#idMensagem").val()
+    };
+
+    $.post("/Home/Cadastrar", parametros)
+
+        .done(function (data) {
+            if (data.status == "OK") {
+                $("#formCadastro").after("<h3>Cadastro realizado com sucesso!</h3>");
+                $("#formCadastro").hide();
+            }
+            else {
+                alert(data.mensagem);
+            }
+        })
+
+        .fail(function () {
+            alert("Erro");
+        })
+}
